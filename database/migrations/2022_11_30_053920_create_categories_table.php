24232b7f96769a184f6588c97324b3dca8cb3122
faruniki <najibfahrunaakbar123@gmail.com>
2022-12-04 14:15:17 +0700

    new commit

---
+++ database/migrations/2022_11_30_053920_create_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique;
+            $table->string('image');
+            $table->date('created_by');
+            $table->date('updated_by');
+            $table->date('deleted_at');
+            $table->date('deleted_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
